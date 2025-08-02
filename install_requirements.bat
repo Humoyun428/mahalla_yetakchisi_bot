a2bf4f14e39eabc0bd201af1936399006db7b75a
Humoyun428 <mhumoyun428@gmail.com>
2025-08-02 23:44:33 +0500

    Add

---
+++ install_requirements.bat
@@ -0,0 +1,4 @@
+@echo off
+echo Kutubxonalar o‘rnatilmoqda...
+C:\Users\Lenovo\PycharmProjects\mahalla_yetakchilari\.venv\Scripts\python.exe -m pip install -r requirements.txt
+pause
---
+++ upgrade_pip.bat
@@ -0,0 +1,3 @@
+@echo off
+C:\Users\Lenovo\PycharmProjects\mahalla_yetakchilari\.venv\Scripts\python.exe -m pip install --upgrade pip setuptools wheel
+pause
